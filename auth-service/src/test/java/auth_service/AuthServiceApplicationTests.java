37045ffaccdd7139793c0a25960efe10aa3c790c
steve <nice_mini@naver.com>
2024-12-21 12:55:44 +0900

    spring boot version 3.1.4로 다운. 이유는 spring cloud가 최신 3.4버전에선 안된다고 함. rombok 에러가 발생해 pom.xml에 전부 버전 추가.

--- auth-service/src/test/java/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/auth_service/AuthServiceApplicationTests.java
@@ -2,8 +2,10 @@ package auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // 테스트 시 application-test.yml 활성화
 class AuthServiceApplicationTests {
 
 	@Test
